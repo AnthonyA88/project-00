0d25621fe06c701c5a6738fbfac3d8940175a95f
AnthonyA88 <Anthonydarata@gmail.com>
2017-10-06 16:26:49 -0700

    More furious

--- game.js
+++ game.js
@@ -1,18 +1,31 @@
 //make a click function for the game to start with a countdown
 console.log("does this work?")
 
-var race = function () {
+$('img').on("click", function()
+
+function() {
 
 	 console.log("Still working?")
 
 }
 //Make starting point for race with countdown
 
+function countDown(start){
+
+	console.log("get ready");
+	setTimeout(function() {
+		alert("Get ready!"); 
+	}, 2000);
+}
+
+
 
 
 
 //Make ending point for race/winning criteria
-
+var winning = function() {
+	//if user clicks car past finish line
+}
 
 
 
