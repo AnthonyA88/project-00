ecbcfc19bda5fdd7dec19732382494cf86da6641
AnthonyA88 <Anthonydarata@gmail.com>
2017-10-06 15:50:38 -0700

    Fast game

---
+++ game.js
@@ -0,0 +1,19 @@
+//make a click function for the game to start with a countdown
+console.log("does this work?")
+
+var race = function () {
+
+	 console.log("Still working?")
+
+}
+//Make starting point for race with countdown
+
+
+
+
+//Make ending point for race/winning criteria
+
+
+
+
+
